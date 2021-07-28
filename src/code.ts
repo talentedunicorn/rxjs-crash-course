0aec6990ec0f5de16c737f850a6aff86276edae8
Talentedunicorn <dev@talentedunicorn.com>
2021-07-28 16:32:17 +0800

    Basic observable usage

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,22 @@
-import * as Rx from 'rxjs';
+import { Observable } from 'rxjs'
+import { addToList } from './lib/domUtils'
 
-console.log(Rx);
+const containerEl = document.getElementById('app')
+const listEl = containerEl.querySelector('ol');
+const listClasses = ['p-4', 'rounded'];
+
+const obervable = Observable.create((observer: any) => {
+  try {
+    observer.next('Hallow')
+    observer.next('Are you there?')
+    observer.complete()
+    observer.next('I will no show up!')
+  } catch (error) {
+    observer.error(error)
+  }
+});
+
+obervable.subscribe(
+  (x: any) => addToList(listEl, x, [...listClasses, 'bg-white']),
+  (error: any) => console.error(error),
+  () => addToList(listEl, 'Completed', [...listClasses, 'bg-green-400', 'text-white']))
