b31cf95bba0b69c52f8a3d7375daeaedf752a7c8
Talentedunicorn <dev@talentedunicorn.com>
2021-07-28 19:35:23 +0800

    multiple observers

--- src/code.ts
+++ src/code.ts
@@ -1,22 +1,29 @@
-import { Observable } from 'rxjs'
+import { Observable, Subscription } from 'rxjs'
 import { addToList } from './lib/domUtils'
 
 const containerEl = document.getElementById('app')
 const listEl = containerEl.querySelector('ol');
 const listClasses = ['p-4', 'rounded'];
 
-const obervable = Observable.create((observer: any) => {
+const observable = Observable.create((observer: any) => {
   try {
     observer.next('Hallow')
     observer.next('Are you there?')
-    observer.complete()
-    observer.next('I will no show up!')
+    setInterval(() => {
+      observer.next('Am i?')
+    }, 1000)
   } catch (error) {
     observer.error(error)
   }
 });
 
-obervable.subscribe(
+const subscription: Subscription = observable.subscribe(
   (x: any) => addToList(listEl, x, [...listClasses, 'bg-white']),
   (error: any) => console.error(error),
   () => addToList(listEl, 'Completed', [...listClasses, 'bg-green-400', 'text-white']))
+
+const subscription2 = observable.subscribe((x: any) => addToList(listEl, x, [...listClasses, 'bg-pink-300']))
+
+subscription.add(subscription2) // Attaching subscription2 to subscription so it gets unsubscribed as well
+
+setTimeout(() => { subscription.unsubscribe() }, 6000)
