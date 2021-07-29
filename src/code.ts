2de4e92ff5c38eb0c98db21b71957ba8160face9
Talentedunicorn <dev@talentedunicorn.com>
2021-07-29 19:07:04 +0800

    mouse events hot subscriptions

--- src/code.ts
+++ src/code.ts
@@ -1,32 +1,14 @@
-import { Observable, share, Subscription } from 'rxjs'
+import { fromEvent } from 'rxjs'
 import { addToList } from './lib/domUtils'
 
 const containerEl = document.getElementById('app')
 const listEl = containerEl.querySelector('ol');
 const listClasses = ['p-4', 'rounded'];
 
-const observable = Observable.create((observer: any) => {
-  try {
-    observer.next('Hallow')
-    observer.next('Are you there?')
-    setInterval(() => {
-      observer.next('Am i?')
-    }, 2000)
-  } catch (error) {
-    observer.error(error)
-  }
-});
-
-const sharedObservable = observable.pipe(share()) // Hot: Output values emitted when the subscription is created - see line 31
-
-const subscription: Subscription = sharedObservable
-  .subscribe(
-    (x: any) => addToList(listEl, x, [...listClasses, 'bg-white']),
-    (error: any) => console.error(error),
-    () => addToList(listEl, 'Completed', [...listClasses, 'bg-green-400', 'text-white']))
-
+const mouseMovements = fromEvent(document, 'mousemove')
 
 setTimeout(() => {
-  // Cold: Output values emitted BEFORE the subscription is created
-  const subscription2 = sharedObservable.subscribe((x:any) => addToList(listEl, x, [...listClasses, 'bg-pink-300']))
-}, 1000)
+  const subsription = mouseMovements.subscribe(
+    ({ x, y }: MouseEvent) => addToList(listEl, `x:${x}, y:${y}`, [...listClasses, 'bg-white'] )
+  )
+}, 5000)
