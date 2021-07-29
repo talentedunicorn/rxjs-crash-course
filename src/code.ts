ec0d0507e3a22d5af10f9e9b84657e2e787be75f
Talentedunicorn <dev@talentedunicorn.com>
2021-07-29 19:31:18 +0800

    Subjects

--- src/code.ts
+++ src/code.ts
@@ -1,14 +1,29 @@
-import { fromEvent } from 'rxjs'
+import { Subject } from 'rxjs'
 import { addToList } from './lib/domUtils'
 
 const containerEl = document.getElementById('app')
 const listEl = containerEl.querySelector('ol');
 const listClasses = ['p-4', 'rounded'];
+const addListItem = (data: string, classes: string[]) => addToList(listEl, data, classes)
 
-const mouseMovements = fromEvent(document, 'mousemove')
+// Can subscribe and emit values
+const subject = new Subject()
 
-setTimeout(() => {
-  const subsription = mouseMovements.subscribe(
-    ({ x, y }: MouseEvent) => addToList(listEl, `x:${x}, y:${y}`, [...listClasses, 'bg-white'] )
-  )
-}, 5000)
+subject.subscribe(
+  (data:string) => addListItem(data, [...listClasses, 'bg-white']),
+  (err) => addListItem(`${err}`, [...listClasses, 'bg-red-200']),
+  () => addListItem('Completed', [...listClasses, 'bg-blue']) 
+)
+
+subject.next('First thing sent!')
+
+const subscription2 = subject.subscribe(
+  (data: string) => addListItem(data, [...listClasses, 'bg-black', 'text-white'])
+)
+
+subject.next('Second thing goes here')
+subject.next('Third thing is cool')
+
+subscription2.unsubscribe()
+
+subject.next('Last thing sent')
