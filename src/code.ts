a7ecf0100cabb6b27bcf1334a63b57f247503418
Talentedunicorn <dev@talentedunicorn.com>
2021-07-29 18:58:06 +0800

    hot and cold subscriptions

--- src/code.ts
+++ src/code.ts
@@ -1,4 +1,4 @@
-import { Observable, Subscription } from 'rxjs'
+import { Observable, share, Subscription } from 'rxjs'
 import { addToList } from './lib/domUtils'
 
 const containerEl = document.getElementById('app')
@@ -11,19 +11,22 @@ const observable = Observable.create((observer: any) => {
     observer.next('Are you there?')
     setInterval(() => {
       observer.next('Am i?')
-    }, 1000)
+    }, 2000)
   } catch (error) {
     observer.error(error)
   }
 });
 
-const subscription: Subscription = observable.subscribe(
-  (x: any) => addToList(listEl, x, [...listClasses, 'bg-white']),
-  (error: any) => console.error(error),
-  () => addToList(listEl, 'Completed', [...listClasses, 'bg-green-400', 'text-white']))
+const sharedObservable = observable.pipe(share()) // Hot: Output values emitted when the subscription is created - see line 31
 
-const subscription2 = observable.subscribe((x: any) => addToList(listEl, x, [...listClasses, 'bg-pink-300']))
+const subscription: Subscription = sharedObservable
+  .subscribe(
+    (x: any) => addToList(listEl, x, [...listClasses, 'bg-white']),
+    (error: any) => console.error(error),
+    () => addToList(listEl, 'Completed', [...listClasses, 'bg-green-400', 'text-white']))
 
-subscription.add(subscription2) // Attaching subscription2 to subscription so it gets unsubscribed as well
 
-setTimeout(() => { subscription.unsubscribe() }, 6000)
+setTimeout(() => {
+  // Cold: Output values emitted BEFORE the subscription is created
+  const subscription2 = sharedObservable.subscribe((x:any) => addToList(listEl, x, [...listClasses, 'bg-pink-300']))
+}, 1000)
