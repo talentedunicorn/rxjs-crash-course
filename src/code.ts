7fc1b2c359325dc98a2e41dc2b3d9a61e9fd4e1b
Talentedunicorn <dev@talentedunicorn.com>
2021-07-30 15:28:55 +0800

    pluck operator

--- src/code.ts
+++ src/code.ts
@@ -1,4 +1,4 @@
-import { Observable, merge } from 'rxjs'
+import { from, pluck } from 'rxjs'
 import { addToList } from './lib/domUtils'
 
 const containerEl = document.getElementById('app')
@@ -6,21 +6,21 @@ const listEl = containerEl.querySelector('ol');
 const listClasses = (bg?: string) => ['p-4', 'rounded-md', bg ? `bg-${bg}`: 'bg-white'];
 const addListItem = (data: string, classes: string[]) => addToList(listEl, data, classes)
 
-const sub1 = new Observable((observer: any) => {
-  observer.next('Hello, Operator')
-})
-
-const sub2 = new Observable((observer: any) => {
-  observer.next('Who is this?')
-})
-
-// This subscription will have values from both sub1 and sub2
-const mergedSub = merge(sub1, sub2)
-
-mergedSub.subscribe(
-  (data: any) => {
-    addListItem(data, listClasses())
+const testData = [
+  {
+    first: 'Lim', last: 'Kee', age: 43,
   },
-)
+  {
+    first: 'Tina', last: 'Turners', age: 32,
+  },
+  {
+    first: 'Lance', last: 'Joy', age: 23,
+  }
+]
 
-sub2.subscribe((data: string) => addListItem(data, listClasses('blue-200')))
+from(testData)
+  .pipe(pluck('first'))
+  .subscribe((first: string) => addListItem(first, listClasses()))
+from(testData)
+  .pipe(pluck('last'))
+  .subscribe((last: string) => addListItem(last, listClasses('gray-300')))
