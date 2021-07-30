976006e1dbd5c6967d1c08210ce8b00a115fa153
Talentedunicorn <dev@talentedunicorn.com>
2021-07-30 14:58:19 +0800

    AsyncSubject

--- src/code.ts
+++ src/code.ts
@@ -1,4 +1,4 @@
-import { ReplaySubject } from 'rxjs'
+import { AsyncSubject } from 'rxjs'
 import { addToList } from './lib/domUtils'
 
 const containerEl = document.getElementById('app')
@@ -6,14 +6,14 @@ const listEl = containerEl.querySelector('ol');
 const listClasses = ['p-4', 'rounded-md'];
 const addListItem = (data: string, classes: string[]) => addToList(listEl, data, classes)
 
-// ReplaySubject allows to set number of events to be buffered
-const subject = new ReplaySubject(3)
+// AsyncSubject only sends last item of stream and when .complete() is called
+const subject = new AsyncSubject()
 subject.next('Testing')
 
 subject.subscribe(
   (data:string) => addListItem(data, [...listClasses, 'bg-white']),
   (err) => addListItem(`${err}`, [...listClasses, 'bg-red-200']),
-  () => addListItem('Completed', [...listClasses, 'bg-blue']) 
+  () => addListItem('Completed', [...listClasses, 'bg-blue-400']) 
 )
 subject.next('First thing sent!')
 subject.next('... warming up observer 2')
@@ -24,7 +24,9 @@ const subscription2 = subject.subscribe(
 
 subject.next('Second thing goes here')
 subject.next('Third thing is cool')
+subject.complete()
 
 subscription2.unsubscribe()
 
 subject.next('Last thing sent')
+subject.complete()
